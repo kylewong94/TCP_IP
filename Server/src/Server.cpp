d66929dfa7b2b5c8062b57759b72dd255359cee6
kylewong94 <kylewong0370@gmail.com>
2019-02-05 03:32:34 -0800

    Changes to Server Class Separating Local and Remote connections. -Created defining functions in Public for Local or Remote -Defined 0 for Local 1 for Remote -Will start on a local test case to help develop structure

--- Server/src/Server.cpp
+++ Server/src/Server.cpp
@@ -1,1 +1,23 @@
 #include "Server.h"
+
+
+Server::Server(int Domain, int Type, int Protocol, int PortID)
+{	
+	printf("Creating socket . . .");
+	if((Socket_FD = socket(IPv4, TCP, 0)) == 0)
+	{
+		printf("Socket failed");
+		exit(-1);
+	}
+	printf("Socket Created . . .");
+}
+
+Server::~Server()
+{
+	delete [] SocketAddr;
+}
+
+int SetSocketOptions(int Level, int OptName, const void * OptVal)
+{
+	if(setsockopt(Socket_FD, Level, 		
+}
--- Server/src/Server.h
+++ Server/src/Server.h
@@ -5,19 +5,34 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define TCP SOCK_STREAM
+#define IPv4 AF_INET
+#define IPv6 AF_INET6
+
+
+#define LOCAL 0
+#define REMOTE 1
+
 class Server
 {
 	private:
-		int Socker_FD;
+		int Socket_FD;
 		int NewSocket;
 		struct SocketAddr * Address;
 		socklen_t OptLen;
 		socklen_t AddrLen;
+		int PortID;
+		int NetworkConnectionType;
 	public:
-		Server(int Domain, int Type, int Protocol);
+		Server(int Domain, int Type, int Protocol, int PortID, int NetworkConnectionType);
 		~Server();
+		int SetSocketOptions(int Level, int OptName, const void * OptVal);
+
+		int Bind_Local();
+		int Listen_Local();
+		int Accept_Local();
+		int Connect_Local();
 	
-		int SetSocketOptions(int Level, int OptName, const void * OptVal) ;
 		int Bind();
 		int Listen(int Backlog);
 		int Accept();
