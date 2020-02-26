8997d7a3bdd8c0ef96f68837dbe1c20815cd70c9
jyji <jyjidev@gmail.com>
2020-02-26 15:29:07 -0800

    fixed some merge issues

--- lib/Client/src/TestCase.cpp
+++ lib/Client/src/TestCase.cpp
@@ -3,14 +3,9 @@
 
 int main (void)
 {
-<<<<<<< HEAD
 	char IPv4 [] = "192.168.137.7";
 	char PortNumber [] = "1025";
 	char HostName  [] = "blackpearl";
-=======
-	char IPv4 [] = "71.92.199.189";
-	char PortNumber [] = "25565";
->>>>>>> d7f1c9f251e681cbe7106f28feca7210c0edcfc1
 
 	Client LocalTest(IPv4, PortNumber);	
 	LocalTest.Connect();
