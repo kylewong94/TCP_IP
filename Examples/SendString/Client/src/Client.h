cc03827f6f96f9e812d76eb73b2f86d26f934e8e
jyji <jyjidev@gmail.com>
2020-03-05 22:38:47 -0800

    played around with a string data server

---
+++ Examples/SendString/Client/src/Client.h
@@ -0,0 +1,30 @@
+#ifndef _SERVER_
+#define _SERVER_ 
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <signal.h>
+
+///////////////////////////////////////////////////////////////////////
+class Client
+{
+	protected:
+		int    LocalSocket;
+		
+		struct addrinfo 	LocalAddr;
+		struct addrinfo *	ServerInfo;
+		struct addrinfo *	ptAddr;
+
+	public:
+		int ERR_FLAG;
+		int RETURN_VAL;
+
+		Client(char * INET4, char * PortNum);
+		~Client();
+
+		int Connect();
+		int SendString(char ** argv, int Len);
+};
+///////////////////////////////////////////////////////////////////////
+
+#endif
---
+++ Examples/SendString/Client/src/TestCase.cpp
@@ -0,0 +1,18 @@
+//#include <fstream>
+#include <string.h>
+#include <stdio.h>
+
+#include "Client.h"
+
+int main (int argc, char ** argv)
+{
+	char IPv4 [] = "127.0.0.1";
+	char PortNumber [] = "1025";
+
+	Client LocalTest(IPv4, PortNumber);	
+	LocalTest.Connect();
+	int Len;
+
+	LocalTest.SendString(argv, argc);
+
+}
